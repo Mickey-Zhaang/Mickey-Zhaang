30524466d65c9681c9dcd64bcf3f2be312f2d312
Mickey_zhaang <qzhang27@colby.edu>
2024-12-29 23:42:29 -0500

    Fixed href erorr of not being able to swap pages by modifying vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ export default defineConfig({
   base: "/Mickey-Zhaang/", 
   build: {
     outDir: "dist", // Output directory for the build
+    rollupOptions: {
+      input: {
+        main: "index.html", 
+        projects: "projects.html", 
+        contact: "contacts.html",
+      },
+    },
   },
 });
